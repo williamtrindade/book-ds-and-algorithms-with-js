d06f826bf9e8916f33240a19189da7bbae2ca99d
William Trindade <williamtrindade777@gmail.com>
2023-04-23 01:22:35 -0300

    Update main.js

--- cap1-js/operadores/main.js
+++ cap1-js/operadores/main.js
@@ -5,3 +5,10 @@ let a = 5; // 5 em binário é 101
 let b = 3; // 3 em binário é 011
 let c = a & b; // c em binário é 001
 console.log(c); // 1
+
+/*
+ * Neste exemplo,
+ * o operador bitwise AND é usado para comparar os bits de a e b.
+ * O resultado é c, que tem um 1 apenas na posição onde ambos a e b têm um 1.
+ * O valor de c é, portanto, 1.
+ */
